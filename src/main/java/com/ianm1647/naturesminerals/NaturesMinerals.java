fe6b15b233fe601115b05521b30bb202a3ac21d4
ianm1647 <ianmcg1128@gmail.com>
2022-05-31 04:30:00 -0500

    Initial commit

---
+++ src/main/java/com/ianm1647/naturesminerals/NaturesMinerals.java
@@ -0,0 +1,15 @@
+package com.ianm1647.naturesminerals;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NaturesMinerals implements ModInitializer {
+	public static final String MODID = "naturesminerals";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
